55f660ecd8be21cc40877f53bcbf2a4d68691951
Arvid Juskaitis <arvydas.juskaitis@gmail.com>
2015-04-05 15:22:49 +0200

    fix menu

--- app/app.js
+++ app/app.js
@@ -1,24 +1,21 @@
 'use strict';
 
-var MyNamespace = MyNamespace || {};
-
-// Define app level helpers
-MyNamespace.helpers = {
-    dummy: function (arg) {
-        return arg;
-    }
-};
-
 // Declare app level module which depends on views, and components
 angular.module('myApp',
-    [
-        'ngRoute',
-        'myApp.log',
-        'myApp.list',
-        'myApp.stats',
-        'myApp.admin',
-        'myApp.version'
-    ])
+        [
+            'ngRoute',
+            'myApp.log',
+            'myApp.list',
+            'myApp.stats',
+            'myApp.admin',
+            'myApp.version'
+        ])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/log'});
-    }]);
+    }])
+    .controller('MenuCtrl', ['$scope', '$location',
+        function ($scope, $location) {
+            $scope.isActive = function (path) {
+                return path == $location.path();
+            };
+        }]);
